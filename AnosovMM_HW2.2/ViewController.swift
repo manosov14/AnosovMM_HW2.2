f0307bcbf011535346781f9f4d6bfa5c44db3997
manosov14 <59836080+manosov14@users.noreply.github.com>
2021-07-08 23:50:37 +0300

    Initial Commit

---
+++ AnosovMM_HW2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnosovMM_HW2.2
+//
+//  Created by Михаил on 08.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
