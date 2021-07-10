bd5cd803974839981a485a5a956418842ad84329
manosov14 <59836080+manosov14@users.noreply.github.com>
2021-07-10 12:42:33 +0300

    finished viewConroller

--- AnosovMM_HW2.2/ViewController.swift
+++ AnosovMM_HW2.2/ViewController.swift
@@ -8,12 +8,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    @IBOutlet var viewer: UIView!
+    
+    @IBOutlet weak var viewArea: UIView!
+    @IBOutlet weak var redColorLabel: UILabel!
+    @IBOutlet weak var greenColorLabel: UILabel!
+    @IBOutlet weak var blueColorLabel: UILabel!
+    
+    @IBOutlet weak var redColorSlider: UISlider!
+    @IBOutlet weak var greenColorSlider: UISlider!
+    @IBOutlet weak var blueColorSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let minValue: Float = 0
+        let maxValue: Float = 1
+        let currentValue: Float = 0.00
+        
+        //redColorSlider
+        redColorSlider.value = currentValue
+        redColorLabel.text = String(redColorSlider.value)
+        redColorSlider.minimumValue = minValue
+        redColorSlider.maximumValue = maxValue
+        redColorSlider.thumbTintColor = .red
+        redColorSlider.minimumTrackTintColor = .red
+        
+        //greenColorSlider
+        greenColorSlider.value = currentValue
+        greenColorSlider.minimumValue = minValue
+        greenColorSlider.maximumValue = maxValue
+        greenColorSlider.thumbTintColor = .green
+        greenColorSlider.minimumTrackTintColor = .green
+        
+        //blueColorSlider
+        blueColorSlider.value = currentValue
+        blueColorSlider.minimumValue = minValue
+        blueColorSlider.maximumValue = maxValue
+        blueColorSlider.thumbTintColor = .blue
+        blueColorSlider.minimumTrackTintColor = .blue
+        
+        //start value for labels
+        redColorLabel.text = String(format: "%.2f", currentValue)
+        greenColorLabel.text = String(format: "%.2f", currentValue)
+        blueColorLabel.text = String(format: "%.2f", currentValue)
+        
+        //start state for view
+        changeColor()
     }
-
-
+    
+    //MARK: - IBActions
+    
+    @IBAction func changeColorAction() {
+        changeColor()
+    }
+    
+    //MARK: -Public methods
+    
+    func changeColor() {
+        viewArea.backgroundColor = UIColor(red: CGFloat(redColorSlider.value), green: CGFloat(greenColorSlider.value), blue: CGFloat(blueColorSlider.value), alpha: 1)
+    }
 }
+
+
+
 
